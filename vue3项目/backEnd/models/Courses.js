826aa6a49a99fad5833718e2359168dd70fa7fca
yang_feng <912057425@qq.com>
2025-03-15 21:48:56 +0800

    修改Coursers

--- vue3项目/backEnd/models/Courses.js
+++ vue3项目/backEnd/models/Courses.js
@@ -16,7 +16,17 @@ module.exports = function (sequelize, DataTypes) {
           model: 'Categories',
           key: 'id'
         },
-        unique: 'fk_Courses_Categories_2'
+        unique: 'fk_Courses_Categories_2',
+        validate: {
+          notNull: { msg: '分类ID必须填写。' },
+          notEmpty: { msg: '分类ID不能为空。' },
+          async isPresent(value) {
+            const category = await sequelize.models.Categories.findByPk(value)
+            if (!category) {
+              throw new Error(`ID为：${value} 的分类不存在。`)
+            }
+          }
+        }
       },
       userId: {
         type: DataTypes.INTEGER.UNSIGNED,
@@ -25,27 +35,57 @@ module.exports = function (sequelize, DataTypes) {
           model: 'Users',
           key: 'id'
         },
-        unique: 'fk_Courses_Users_1'
+        unique: 'fk_Courses_Users_1',
+        validate: {
+          notNull: { msg: '用户ID必须填写。' },
+          notEmpty: { msg: '用户ID不能为空。' },
+          async isPresent(value) {
+            const user = await sequelize.models.Users.findByPk(value)
+            if (!user) {
+              throw new Error(`ID为：${value} 的用户不存在。`)
+            }
+          }
+        }
       },
       name: {
         type: DataTypes.STRING(255),
-        allowNull: false
+        allowNull: false,
+        validate: {
+          notNull: { msg: '名称必须填写。' },
+          notEmpty: { msg: '名称不能为空。' },
+          len: { args: [2, 45], msg: '名称长度必须是2 ~ 45之间。' }
+        }
       },
       image: {
         type: DataTypes.STRING(255),
-        allowNull: true
+        allowNull: true,
+        validate: {
+          isUrl: { msg: '图片地址不正确。' }
+        }
       },
       recommended: {
         type: DataTypes.TINYINT,
         allowNull: true,
         defaultValue: 0,
-        unique: 'recommended'
+        unique: 'recommended',
+        validate: {
+          isIn: {
+            args: [[0, 1]],
+            msg: '是否推荐的值必须是，推荐：1 不推荐：0。'
+          }
+        }
       },
       introductory: {
         type: DataTypes.TINYINT,
         allowNull: true,
         defaultValue: 0,
-        unique: 'introductory'
+        unique: 'introductory',
+        validate: {
+          isIn: {
+            args: [[0, 1]],
+            msg: '是否入门课程的值必须是，推荐：1 不推荐：0。'
+          }
+        }
       },
       content: {
         type: DataTypes.TEXT,
