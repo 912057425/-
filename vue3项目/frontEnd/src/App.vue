472d141b7f78a67d21f445b6059840da79c02435
yang_feng <912057425@qq.com>
2025-04-11 16:43:06 +0800

    前端项目框架

---
+++ vue3项目/frontEnd/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- 路由出口，用于显示当前路由对应的组件 -->
+  <div class="common-layout">
+    <header>12132</header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
